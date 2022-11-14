c8c8c1ff40582dae55a232fc6d9486aa9a04a171
fpaniaguaangular <fernando.paniagua.java@gmail.com>
2022-11-14 18:42:50 +0100

    Procesado JSON

--- src/main/java/com/fernandopaniagua/ejemplohttp/App.java
+++ src/main/java/com/fernandopaniagua/ejemplohttp/App.java
@@ -4,6 +4,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class App {
 
 	public static void main(String[] args) {
@@ -13,11 +18,35 @@ public class App {
 			Path path = Paths.get("d:/salida.html");
 			Files.write(path, respuesta.getBytes());
 			System.out.println(respuesta);
+			procesarJSON(respuesta);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			System.out.println("FIN");
 		}
 	}
-
+	
+	public static void procesarJSON(String strPelicula) {
+		JSONParser parser = new JSONParser();
+		try {
+			JSONObject jsonPelicula = (JSONObject)parser.parse(strPelicula);
+			System.out.println("JSON proceso completo");
+			
+			String title = (String)jsonPelicula.get("Title");
+			String year = (String)jsonPelicula.get("Year");
+			String director = (String)jsonPelicula.get("Director");
+			System.out.println(title + ":" + year + ":" + director); 
+			
+			JSONArray ratings = (JSONArray)jsonPelicula.get("Ratings");
+			for (Object rating : ratings) {
+				String source = (String)((JSONObject)rating).get("Source");
+				String value = (String)((JSONObject)rating).get("Value");
+				System.out.println("Fuente:" + source + " - Calificación:" + value);
+			}
+			
+			
+		} catch (ParseException pe) {
+			pe.printStackTrace();
+		}
+	}
 }
--- src/main/java/com/fernandopaniagua/ejemplohttp/ClienteHTTP.java
+++ src/main/java/com/fernandopaniagua/ejemplohttp/ClienteHTTP.java
@@ -8,6 +8,7 @@ import java.net.URL;
 public class ClienteHTTP {
 	public static String getStringFromURL(String strURL) throws Exception  {
 		StringBuilder respuesta = new StringBuilder();//StringBuilder --> Mejora la concatenación de String
+		//StringBuffer respuesta = new StringBuffer();//Similar a StringBuilder pero resistente a threads
 		URL url = new URL(strURL);//URL identifica un recurso en internet
 		HttpURLConnection conexion = (HttpURLConnection)url.openConnection();//Abrir conexión
 		//Configuración de la conexión
@@ -15,7 +16,7 @@ public class ClienteHTTP {
 		//Se pueden añadir más parámetros (tipo de retorno, juego de caracteres, agente de usuario...)
 		//Establecer la conexión
 		int code = conexion.getResponseCode();
-		if (code==HttpURLConnection.HTTP_OK){
+		if (code==HttpURLConnection.HTTP_OK){//HTTP_OK == 200
 			System.out.println("La petición se ha procesado correctamente");
 			//Abierto el canal de lectura
 			Reader reader = new InputStreamReader(conexion.getInputStream());
